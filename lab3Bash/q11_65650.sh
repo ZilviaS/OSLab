dec7aadb62df92d79df5084c5ed7b7fc81ec6100
ZilviaS <113342968+ZilviaS@users.noreply.github.com>
2023-08-13 18:36:17 +0700

    Add files via upload

---
+++ lab3Bash/q11_65650.sh
@@ -0,0 +1,19 @@
+for file in $(find . -type f -name "L*.c") 
+do 	
+trimmedName=`echo $file | cut -d'/' -f2 | cut -d'.' -f1`
+prog_name=$(echo $trimmedName | cut -d'.' -f1)
+gcc -o $prog_name $trimmedName 2>/dev/null
+if [ -f $prog_name ]
+then
+	ans=$(./prog_name)
+	if [ $ans -eq 20 ]
+	then
+		score=10
+	else
+		score=7
+	fi
+else
+	score=5
+fi
+printf "%s\t\t%d\n" $trimmedName $score	
+done 
